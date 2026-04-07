5de9230c3f47de3d300648c0683f995b61831845
Mikhail-go <sharakmp@yandex.ru>
2026-04-07 15:12:58 +0300

    --help

--- mbus.go
+++ mbus.go
@@ -32,7 +32,6 @@ func Cserial(portname string, baud int, stopb byte, prt byte) error {
 	return er
 }
 func (p *Dfom) Getrtu(sadr, nw uint16, mba byte) (error) {
-	//m-длина принятого сообщения включая КС
 	var cs1, cs2 uint16
 	b := make ([]byte, 128)
 	b[0] = mba		//modbus адрес OM310
@@ -41,22 +40,11 @@ func (p *Dfom) Getrtu(sadr, nw uint16, mba byte) (error) {
 	b[3] = byte(sadr)	// младший байт начального адреса
 	b[4] = byte(nw>>8)	// старший байт числа слов
 	b[5] = byte(nw)		// младший байт числа слов
-	//lm = int(b[5]<<1+3+2) // должно быть принято байт от ОМ310 в режиме "чтение"	
-	cs1 =Crcsum(b,6)
-	b[6] = byte (cs1)
-	b[7] = byte (cs1>>8)
-	bm := b[8:] 	//сюда принимаем ответ
+    bm := b[6:] 	//сюда принимаем ответ
 	m, err := Getoms(b) //посылаем команду и получаем ответ
 	if m == 0 || err != nil {
             fmt.Println(err)  
         return err
-	}
-	cs2 =Crcsum (bm, m-2)
-	cs1 = uint16(bm[m-1])
-	cs1 = (cs1<<8)^uint16(bm[m-2])
-	if (cs1^cs2)!=0 {
-	err = fmt.Errorf ("Ошибка КС при приёме: %d %d",cs1,cs2)
-	return err	
 	}
 	nad := int(bm[2]) // длина блока полученных данных в байтах в режиме чтения
     ara := *p
@@ -74,7 +62,6 @@ func (p *Dfom) Getrtu(sadr, nw uint16, mba byte) (error) {
 // чтение данных из ОМ310 по сетевому соединению
 func (p *Dfom) Gettcp(sadr, nw uint16, conn net.Conn, mad byte) (error) {
 	var m, lm int	 //lm-длина принятого сообщения включая КС
-	var cs1, cs2 uint16
 	b := make ([]byte, 128)
 	b[0] = mad      //1		//физ. адрес OM310
 	b[1] = 3		//режим чтения
@@ -82,30 +69,20 @@ func (p *Dfom) Gettcp(sadr, nw uint16, conn net.Conn, mad byte) (error) {
 	b[3] = byte(sadr)	// младший байт начального адреса
 	b[4] = byte(nw>>8)	// старший байт числа слов данных
 	b[5] = byte(nw)		// младший байт числа слов
-	lm = int(b[5]<<1+3+2) // должно быть принято байт от ОМ310 в режиме "чтение"	
-	cs1 =Crcsum(b,6)
-	b[6] = byte (cs1)
-	b[7] = byte (cs1>>8)
-	bm := b[8:] 	//сюда принимаем ответ
-	// отправляем сообщение серверу
-        n, err := conn.Write(b[:8])
+    lm = int(b[5]<<1+3) // должно быть принято байт от ОМ310 в режиме "чтение"	
+    bm := b[6:] 	//сюда принимаем ответ
+	// отправляем команду серверу
+    n, err := conn.Write(b[:6])
 	if n == 0 || err != nil { 
             fmt.Println(err)  
             return err
 	}
 	// приём ответа
-	if m, err = conn.Read(bm[:lm]); (m < 4 || err !=nil) {
+	if m, err = conn.Read(bm[:lm]); (m < 5 || err !=nil) {
 		fmt.Println(err)
 		return err 
 	}
-	cs2 =Crcsum (bm, m-2)
-	cs1 = uint16(bm[m-1])
-	cs1 = (cs1<<8)^uint16(bm[m-2])
-	if (cs1^cs2)!=0 {
-	    err = fmt.Errorf ("Ошибка КС при приёме: %d %d",cs1,cs2)
-	return err	
-	}
-	nad := int(bm[2]) // длина блока полученных данных в байтах в режиме чтения
+	nad := int(bm[2]) // длина блока полученных данных в байтах в режиме чтения 
 	*p = nil	//сброс в начало для повторного использования одного и того же имени среза
 	for i := 0; i < nad ; i++ { 
 		x := uint16(bm[3+i])
@@ -117,8 +94,6 @@ func (p *Dfom) Gettcp(sadr, nw uint16, conn net.Conn, mad byte) (error) {
 	return nil
 }
 func (p *Dfom) Getrtua(sadr, nw uint16, mba byte) (error) {
-	//m-длина принятого сообщения включая КС
-	var cs1, cs2 uint16
 	b := make ([]byte, 128)
 	b[0] = mba		//физ. адрес OM310
 	b[1] = 3		//режим чтения
@@ -126,22 +101,11 @@ func (p *Dfom) Getrtua(sadr, nw uint16, mba byte) (error) {
 	b[3] = byte(sadr)	// младший байт начального адреса
 	b[4] = byte(nw>>8)	// старший байт числа слов
 	b[5] = byte(nw)		// младший байт числа слов
-	//lm = int(b[5]<<1+3+2) // должно быть принято байт от ОМ310 в режиме "чтение"	
-	cs1 =Crcsum(b,6)
-	b[6] = byte (cs1)
-	b[7] = byte (cs1>>8)
-	bm := b[8:] 	//сюда принимаем ответ
+    bm := b[6:] 	//сюда принимаем ответ
 	m, err := Getoms(b) //посылаем команду и получаем ответ
 	if m == 0 || err != nil {
            	fmt.Println(err)  
         return err
-	}
-	cs2 = Crcsum (bm, m-2)
-	cs1 = uint16(bm[m-1])
-	cs1 = (cs1<<8)^uint16(bm[m-2])
-	if (cs1^cs2)!=0 {
-	err = fmt.Errorf ("Ошибка КС при приёме: %d %d",cs1,cs2)
-	return err	
 	}
 	nad := int(bm[2]) // длина блока полученных данных в байтах в режиме чтения
     ara := *p
@@ -157,8 +121,6 @@ func (p *Dfom) Getrtua(sadr, nw uint16, mba byte) (error) {
 	return nil
 }
 func Putrtu(sadr, d uint16, mba byte) (error) {
-	//var m, lm int	 //lm-длина принятого сообщения включая КС
-	var cs1 uint16
 	b := make ([]byte, 32)
 	b[0] = mba
 	b[1] = 	6	// код команды "запись 1 слова"
@@ -166,24 +128,24 @@ func Putrtu(sadr, d uint16, mba byte) (error) {
 	b[3] = byte(sadr)	// младший байт начального адреса
 	b[4] = 0		// старший байт данных
 	b[5] = byte(d)		// и младший 
-	//lm = 8	//длина ответа в байтах в режиме "запись 1 слова"
-	cs1 = Crcsum(b,6)
-	b[6] = byte (cs1)
-	b[7] = byte (cs1>>8)
 	m, err := Getoms (b) //посылаем команду и получаем ответ
 	if m == 0 || err != nil { 
             fmt.Println(err)  
             return err
 	}
-	return nil //при записи параметра, ОМ310 возвращает только саму команду (без данных)
+	return nil //при записи параметра, ОМ310 возвращает обратно только те же 6 байт
 }
 //отправляем команду и принимаем ответ (modbus rtu ) 
 func Getoms(b []byte) (int, error) {
 	Omact <- true // синхронизация с ограничителем частоты обращения к ОМ310
 	var er error
 	var m, m1, n int
+    var cs1, cs2 uint16
 	ms := 50 //задержка  в режиме чтения (миллисекунды) //50
-	bm := b[8:] 
+    cs1 = Crcsum(b,6)   //вычисляем КС команды
+    b[6] = byte(cs1)
+    b[7] = byte (cs1 >> 8)
+	bm := b[8:]
 	lm := int(b[5]<<1+3+2) //ожидаемая длина при приёме из ОМ310 +КС
 	if b[1] == 6 {
 		lm = 8  // 8 байт длина ответа в режиме записи +КС
@@ -196,23 +158,38 @@ func Getoms(b []byte) (int, error) {
 	time.Sleep(time.Millisecond * time.Duration(ms)) // задержка на передачу команды из 8 байт ! //50
 
 	if m, er = S.Read (bm[:lm]); (er !=nil) {
-		return 0, er
+		return 0, er //
 	}
 	if m != lm {
-	fmt.Println("lm, m :", lm, m)
+	fmt.Println("lm, m :", lm, m) //отладка
+        if m == 5 && ((bm[1] >> 7) == 1) {
+        er = fmt.Errorf("неверная Modbus-команда от клиента, код ошибки (hex) %x", bm[2])
+        return 0, er
+        }
 	//если приняли не всё - считываем остаток
 	bm1 := bm[m:]
 	m1 = lm - m
 	n, er = S.Read(bm1[:m1])
 	m = lm
-	if n != m1 {m = 0 } // нет ничего, это сбой
+	    if n != m1 {
+        er = fmt.Errorf("сбой протокола Modbus ")
+        return 0, er
+        }
 	}
-	return m, nil
+    m1 = m-2
+    er = nil
+    cs2 = Crcsum(bm, m1)
+    cs1 = uint16(bm[m-1])
+    cs1 = (cs1<<8) ^ uint16(bm[m-2])
+    if (cs1 ^ cs2) != 0 {
+    er = fmt.Errorf("Ошибка КС при приёме")
+    m1 = 0
+    } 
+	return m1, er //при успехе возвращаем m1 байт (без КС)
 }
 // Запись данных по modbus tcp (отсылка команды)
 func Puttcp(sadr, d uint16, conn net.Conn, mad byte) (error) {
 	var m, lm int	 //lm-длина принятого сообщения включая КС
-	var cs1, cs2 uint16
 	b := make ([]byte, 64)
 	b[0] = mad      //1
 	b[1] = 	6	// код команды "запись 1 слова"
@@ -220,29 +197,19 @@ func Puttcp(sadr, d uint16, conn net.Conn, mad byte) (error) {
 	b[3] = byte(sadr)	// младший байт начального адреса
 	b[4] = byte(d>>8)   // старший байт данных
 	b[5] = byte(d)      // и младший 
-	lm = 8	//длина ответа в байтах в режиме "запись 1 слова"
-	cs1 =Crcsum(b,6)
-	b[6] = byte (cs1)
-	b[7] = byte (cs1>>8)
-	bm := b[8:] 	//сюда принимаем ответ
-	// отправляем его серверу
-        n, err := conn.Write(b[:8])
+    lm = 6
+	bm := b[6:] 	//сюда принимаем ответ
+	// отправляем команду серверу
+        n, err := conn.Write(b[:6])
 	if n == 0 || err != nil { 
             fmt.Println(err)  
             return err
 	}
 	// приём ответа
-	if m, err = conn.Read(bm[:lm]); (m < 4 || err !=nil) {
+	if m, err = conn.Read(bm[:lm]); (m < 5 || err !=nil) {
 		fmt.Println(err)
 		return err 
 	}
-	cs2 =Crcsum (bm, m-2)
-	cs1 = uint16(bm[m-1])
-	cs1 = (cs1<<8)^uint16(bm[m-2])
-	if (cs1^cs2)!=0 {
-	    err = fmt.Errorf ("Ошибка КС при приёме: %d %d",cs1,cs2)
-	return err	
-	}	 
 	return nil
 }
 func Crcsum(b []byte, nb int) uint16 {
@@ -259,15 +226,4 @@ func Crcsum(b []byte, nb int) uint16 {
 	}
 	return c
 }
-func Crctest( b []byte, nb int) uint16 {
-	cs1 := Crcsum(b, nb) //!
-	cs2 := uint16(b[nb+1])
-	cs2 = cs2<<8|uint16(b[nb])
-	return cs1^cs2
-}
-//func crcput(b []byte, nb int) {
-//	cs1 := Crcsum(b, nb)
-//	b[nb] = byte(cs1)
-//	b[nb+1] = byte(cs1 >>8)
-//}
 
